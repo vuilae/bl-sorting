98662efcc844be870dd1b0d9ee691a14190c76f5
Дарина <darina.bakeeva2005@gmail.com>
2024-04-20 13:21:55 +0500

    snt

--- src/lib.rs
+++ src/lib.rs
@@ -1,108 +1,87 @@
 
-use std::cmp::Ord;
-
-
-pub fn quick_sort<T>(mut data: Vec<T>) -> Vec<T>
-where
-    T: Ord + Clone,
-{
-    if data.len() <= 1 {
-        return data;
+pub fn quick_sort<T: Ord>(arr: &mut [T]) {
+    if arr.len() <= 1 {
+        return;
     }
-
-    let pivot_index = partition(&mut data);
-    let (left, right) = data.split_at(pivot_index);
-
-    let mut left_sorted = quick_sort(left.to_vec());
-    left_sorted.append(&mut quick_sort(right.to_vec()));
-
-    left_sorted
+    let pivot = partition(arr);
+    quick_sort(&mut arr[0..pivot]);
+    quick_sort(&mut arr[pivot + 1..]);
 }
 
-pub fn selection_sort<T>(mut data: Vec<T>) -> Vec<T>
-where
-    T: Ord + Clone,
-{
-    for i in 0..data.len() - 1 {
-        let mut min_index = i;
-        for j in i + 1..data.len() {
-            if data[j] < data[min_index] {
-                min_index = j;
-            }
-        }
-        data.swap(i, min_index);
-    }
-
-    data
-}
-
-pub fn insertion_sort<T>(mut data: Vec<T>) -> Vec<T>
-where
-    T: Ord + Clone,
-{
-    for i in 1..data.len() {
-        let mut key = data[i].clone();
-        let mut j = i - 1;
-
-        while j >= 0 && data[j] > key {
-            data[j + 1] = data[j].clone();
-            j -= 1;
-        }
-
-        data[j + 1] = key;
-    }
-
-    data
-}
-
-pub fn merge_sort<T>(mut data: Vec<T>) -> Vec<T>
-where
-    T: Ord + Clone,
-{
-    if data.len() <= 1 {
-        return data;
-    }
-
-    let mid = data.len() / 2;
-    let left = merge_sort(data[..mid].to_vec());
-    let right = merge_sort(data[mid..].to_vec());
-
-    merge(left, right)
-}
-
-fn merge<T>(mut left: Vec<T>, mut right: Vec<T>) -> Vec<T>
-where
-    T: Ord + Clone,
-{
-    let mut result = Vec::new();
-    while !left.is_empty() && !right.is_empty() {
-        if left[0] < right[0] {
-            result.push(left.remove(0));
-        } else {
-            result.push(right.remove(0));
-        }
-    }
-
-    result.append(&mut left);
-    result.append(&mut right);
-
-    result
-}
-
-fn partition<T>(data: &mut Vec<T>) -> usize
-where
-    T: Ord + Clone,
-{
-    let pivot = data.last().unwrap().clone();
+fn partition<T: Ord>(arr: &mut [T]) -> usize {
+    let pivot = arr.len() - 1;
     let mut i = 0;
-
-    for j in 0..data.len() - 1 {
-        if data[j] <= pivot {
-            data.swap(i, j);
+    for j in 0..pivot {
+        if arr[j] <= arr[pivot] {
+            arr.swap(i, j);
             i += 1;
         }
     }
-
-    data.swap(i, data.len() - 1);
+    arr.swap(i, pivot);
     i
+}
+
+pub fn selection_sort<T: Ord>(arr: &mut [T]) {
+    for i in 0..arr.len() {
+        let mut min_index = i;
+        for j in i + 1..arr.len() {
+            if arr[j] < arr[min_index] {
+                min_index = j;
+            }
+        }
+        if i != min_index {
+            arr.swap(i, min_index);
+        }
+    }
+}
+
+pub fn insertion_sort<T: Ord>(arr: &mut [T]) {
+    for i in 1..arr.len() {
+        let mut j = i;
+        while j > 0 && arr[j - 1] > arr[j] {
+            arr.swap(j - 1, j);
+            j -= 1;
+        }
+    }
+}
+
+pub fn merge_sort<T: Ord + Clone + Copy>(arr: &mut [T]) {
+    if arr.len() <= 1 {
+        return;
+    }
+    let mid = arr.len() / 2;
+    merge_sort(&mut arr[..mid]);
+    merge_sort(&mut arr[mid..]);
+    let mut merged = arr.to_vec();
+    merge(&arr[..mid], &arr[mid..], &mut merged);
+    arr.copy_from_slice(&merged);
+}
+
+fn merge<T: Ord + Clone>(left: &[T], right: &[T], merged: &mut [T]) {
+    let mut left_index = 0;
+    let mut right_index = 0;
+    let mut merged_index = 0;
+
+    while left_index < left.len() && right_index < right.len() {
+        if left[left_index] <= right[right_index] {
+            merged[merged_index] = left[left_index].clone();
+            left_index += 1;
+        } else {
+            merged[merged_index] = right[right_index].clone();
+            right_index += 1;
+        }
+        merged_index += 1;
+    }
+
+    while left_index < left.len() {
+        merged[merged_index] = left[left_index].clone();
+        left_index += 1;
+        merged_index += 1;
+    }
+
+    while right_index < right.len() {
+        merged[merged_index] = right[right_index].clone();
+        right_index += 1;
+        merged_index += 1;
+    }
 }
